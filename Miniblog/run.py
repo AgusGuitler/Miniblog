b08bf17fa2550d27573c2be2648e59929a8433f6
root <root@DESKTOP-UV5EVBB.localdomain>
2022-06-23 22:06:22 -0300

    User login

--- Miniblog/run.py
+++ Miniblog/run.py
@@ -1,14 +1,21 @@
 #from crypt import methods
+from crypt import methods
 import email
 from email.policy import default
 from operator import ne
 from turtle import pos, title
 from flask import Flask
 from flask import render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, current_user
+from Miniblog.forms import LoginForm
 from forms import SignupForm, PostForm
+from models import get_user, users
+from werkzeug.urls import url_parse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
+login_manager = LoginManager(app)
 
 posts = []
 
@@ -48,3 +55,25 @@ def show_signup_form():
             return redirect(next)
         return redirect(url_for("index"))
     return render_template("signup_form.html", form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login_form.html', form=form)
